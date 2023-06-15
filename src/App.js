0820c639cc5c315e027b62e9c3a7abfd25ef32b1
Arpit Jana <arpitjana2103@gmail.com>
2023-06-15 19:51:19 +0530

    Inital commit : React App created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = function() {
+  const name = "John";
+
+    return (
+        <div className="App">
+            <h1>Hello {name} !</h1>
+        </div>
+    );
+}
+
+export default App;
